a38a5a2a6333f45661ae5e048b62b169f6afed08
jeongtai <zzze37@naver.com>
2022-03-01 23:54:52 +0900

    Complit contract locally and run

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-contract WavePortal11 {
+contract WavePortal {
     constructor() {
         console.log("YOU KNOW WHAT I AMwwwww SAYING IT?");
     }
